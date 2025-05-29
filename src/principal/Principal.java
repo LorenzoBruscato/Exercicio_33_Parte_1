5dd01977e6a6ca25c694b9874ff8e7d4503b4fa7
Lorenzo Bruscato <168117961+LorenzoBruscato@users.noreply.github.com>
2025-05-28 23:20:49 -0300

    Update Principal.java

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
 
 import javax.swing.*;
@@ -9,6 +5,10 @@ import model.Fisica;
 import model.Juridica;
 import model.Empresa;
 
+/**
+ *
+ * @author Lorenzo
+ */
 public class Principal {
 
     public static void main(String args[]) {
